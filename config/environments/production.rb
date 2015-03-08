9ba5efa014655575826f59b5523d25e525cac54a
Stephen Baines <Stephen@Stephens-MacBook-Air.local>
2015-03-08 19:57:17 +0000

    upload

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['sb-pinteresting'],
+      :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
