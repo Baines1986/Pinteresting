640d7e17e4b566127679d82d9bbdd3d90fc6b6ca
Stephen Baines <Stephen@Stephens-MacBook-Air.local>
2015-03-22 10:41:54 +0000

    updated the show view and the icons

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   # get "pages/home"
 
-  root "pages#home"
+  root "pins#index"
 
   get "information" => "pages#information"
   get "about" => "pages#about"
